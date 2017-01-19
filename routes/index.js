829fd876ab3e12f99ff809b76202312f96808d9f
Stephanie Cure <e.cure.ch@gmail.com>
2017-01-19 17:52:24 -0500

    completed!

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ module.exports = function makeRouterWithSockets (io) {
   // a reusable function
   function respondWithAllTweets (req, res, next){
     //Querying the tweets
-    client.query('SELECT tweets.content, users.name FROM tweets JOIN users ON users.id=tweets.user_id', function(err, result){
+    client.query('SELECT * FROM tweets JOIN users ON users.id=tweets.user_id', function(err, result){
       if(err){return next(err)}
       else{
         var tweet = result.rows;
@@ -46,7 +46,7 @@ module.exports = function makeRouterWithSockets (io) {
   // single-tweet page
   router.get('/tweets/:id', function(req, res, next){
     var tweetId = Number(req.params.id);
-    console.log(req.params);
+    //console.log(req.params);
     client.query('SELECT * FROM tweets JOIN users ON users.id = tweets.user_id WHERE tweets.id = $1', [tweetId], function(err, result){
       if(err){return next(err)}
       else {
@@ -61,9 +61,18 @@ module.exports = function makeRouterWithSockets (io) {
 
   // create a new tweet
   router.post('/tweets', function(req, res, next){
-    var newTweet = data.add(req.body.name, req.body.content);
-    io.sockets.emit('new_tweet', newTweet);
-    res.redirect('/');
+    var name = req.body.name;
+    var content = req.body.content;
+    client.query("INSERT INTO tweets(user_id, content) VALUES((SELECT id FROM users WHERE name=$1),$2) RETURNING user_id, content",[name, content], function(err, result){
+      var newTweet = result.rows;
+
+      console.log(newTweet)
+      io.sockets.emit('new_tweet', newTweet);
+      res.redirect('/');
+    });
+
+    //var newTweet = data.add(req.body.name, req.body.content);
+
   });
 
   // // replaced this hard-coded route with general static routing in app.js
