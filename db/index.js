00fc328aab5a84320d5c3a531f840c9eb8a8d475
Stephanie Cure <e.cure.ch@gmail.com>
2017-01-19 15:40:16 -0500

    second commit

--- db/index.js
+++ db/index.js
@@ -6,22 +6,7 @@ var postgresUrl = 'postgres://localhost/twitterdb';
 var client = new pg.Client(postgresUrl)
 
 // connect to our database
-client.connect(function (err) {
-  if (err) throw err;
-
-  // execute a query on our database
-  client.query('SELECT * FROM users', function (err, result) {
-    if (err) throw err;
-
-    // just print the result to the console
-    console.log(result.rows[0]); // outputs: { name: 'brianc' }
-
-    // disconnect the client
-    client.end(function (err) {
-      if (err) throw err;
-    });
-  });
-});
+client.connect();
 
 module.exports = client;
 
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,42 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-var tweetBank = require('../tweetBank');
+//var tweetBank = require('../tweetBank');
+var client = require('../db/index');
 
 module.exports = function makeRouterWithSockets (io) {
 
+  //db test
+//   var usersName = client.query('SELECT name FROM users', function (err, result) {
+//   if (err) {return next(err);}
+//   else {
+//     var names = result.rows;
+//     console.log(names)
+//      names.forEach(person =>{
+//     //return(names.name);
+//     console.log(person.name);
+//   })
+//   }// pass errors to Express
+
+
+//   // console.log(tweets)
+// });
+
   // a reusable function
   function respondWithAllTweets (req, res, next){
-    var allTheTweets = tweetBank.list();
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: allTheTweets,
-      showForm: true
-    });
-  }
+    client.query('SELECT name FROM users', function (err, result) {
+     if (err) {return next(err);}
+    else {
+        var names = result.rows;
+        res.render('index', {
+        title: 'Twitter.js',
+        tweets: names,
+        showForm: true
+      });
+    }
+  })
+}
+
 
   // here we basically treet the root view and tweets view as identical
   router.get('/', respondWithAllTweets);
@@ -21,18 +44,24 @@ module.exports = function makeRouterWithSockets (io) {
 
   // single-user page
   router.get('/users/:username', function(req, res, next){
-    var tweetsForName = tweetBank.find({ name: req.params.username });
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: tweetsForName,
-      showForm: true,
-      username: req.params.username
+    var tweetsForName = client.query("SELECT users.name, tweets.content FROM users JOIN tweets ON tweets.user_id=users.id" , function(err, result){
+      if(err){
+        return next(err);
+      }else{
+        res.render('index', {
+        title: 'Twitter.js',
+        tweets: result.row(tweetsForName),
+        showForm: true,
+        username: req.params.username
+    });
+      }
+      console.log(results.rows)
     });
   });
 
   // single-tweet page
   router.get('/tweets/:id', function(req, res, next){
-    var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
+    var tweetsWithThatId = data.find({ id: Number(req.params.id) });
     res.render('index', {
       title: 'Twitter.js',
       tweets: tweetsWithThatId // an array of only one element ;-)
@@ -41,7 +70,7 @@ module.exports = function makeRouterWithSockets (io) {
 
   // create a new tweet
   router.post('/tweets', function(req, res, next){
-    var newTweet = tweetBank.add(req.body.name, req.body.content);
+    var newTweet = data.add(req.body.name, req.body.content);
     io.sockets.emit('new_tweet', newTweet);
     res.redirect('/');
   });
